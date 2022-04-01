7d32002149e65a8c6f29029919da5876af8be5d7
dddooo9 <dlaehdus99@naver.com>
2022-04-01 20:02:14 +0900

    2주차 과제

--- app/src/main/java/com/doyeon/flo/MainActivity.kt
+++ app/src/main/java/com/doyeon/flo/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.doyeon.flo
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.doyeon.flo.databinding.ActivityMainBinding
@@ -13,17 +14,28 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        initBottomNavigation()
+        val song = Song(
+            binding.mainMiniPlayerTitleTv.text.toString(),
+            binding.mainMiniPlayerSingerTv.text.toString()
+        )
 
+        binding.mainPlayerCl.setOnClickListener {
+            val intent = Intent(this, SongActivity::class.java)
+            intent.putExtra("title", song.title)
+            intent.putExtra("singer", song.singer)
+            startActivity(intent)
+        }
+
+        initBottomNavigation()
     }
 
-    private fun initBottomNavigation(){
+    private fun initBottomNavigation() {
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.main_frm, HomeFragment())
             .commitAllowingStateLoss()
 
-        binding.mainBnv.setOnItemSelectedListener{ item ->
+        binding.mainBnv.setOnItemSelectedListener { item ->
             when (item.itemId) {
 
                 R.id.homeFragment -> {
